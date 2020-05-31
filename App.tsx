c5977b0a3b3afabd24fdd828ed6027796a396db9
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-05-31 13:30:23 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  let x = 0;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Opxsdbfdvf! {x}</Text>
+      <Button
+        onPress={() => {
+          x = x + 1;
+        }}
+        title="click me"
+        accessibilityLabel="couasdnter"
+      ></Button>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: Platform.OS === "android" ? 35 : 0,
   },
 });
